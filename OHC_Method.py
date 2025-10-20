ac3e422ccc4279c8e2fc831c2e110cc9ccd328cd
oussemajerfel12 <oussemajerfel@gmail.com>
2025-10-20 13:36:26 +0100

    Update Code

--- OHC_Method.py
+++ OHC_Method.py
@@ -1,5 +1,5 @@
 import argparse
-from pathlib import path
+from pathlib import Path
 import xarray as xr 
 import numpy as np  
 import matplotlib.pyplot as plt
@@ -8,5 +8,15 @@ def load_data(mask_file_wo13, mask_file, temperature_file):
     ds_mask_file_wo13 = xr.open_dataset(mask_file_wo13)
     ds_mask = xr.open_dataset(mask_file)
     ds_temp = xr.open_dataset(temperature_file)
+    print(ds_temp)
 
     return ds_mask_file_wo13,ds_mask,ds_temp
+
+if __name__ == "__main__":
+    mask_file_woa13 = "blue-cloud-dataspace/MEI/INGV/INPUT/BATHYMETRY/gebco_2019_mask_1_8_edited_final_woa13.nc"
+    mask_file = "blue-cloud-dataspace/MEI/INGV/INPUT/BATHYMETRY/gebco_2019_mask_1_8_edited_final.nc"
+    temperature_file = 'blue-cloud-dataspace/MEI/INGV/INPUT/CLIMATOLOGY/Temperature_sliding_climatology_WP.nc'
+
+    load_data(mask_file_woa13,mask_file,temperature_file)
+
+
